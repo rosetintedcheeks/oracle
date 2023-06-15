f39565fa3924cc4e2ca4b1c3aa9f25f68d2c5f15
rosetintedcheeks <oaks@rosetintedcheeks.com>
2023-06-14 21:36:29 -0400

    Forgot a file xd

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
-git pull
-composer install
-npm install
+composer install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader
+npm install --production
 npm run build
+php artisan config:cache
+php artisan view:cache
 php artisan migrate
